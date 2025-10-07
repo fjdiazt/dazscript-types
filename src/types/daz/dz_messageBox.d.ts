6de5c732dceb1ff3883561ff2a9d9653998f6ebb
Freddy Diaz <fjdiazt@outlook.com>
2025-10-06 23:48:21 -0300

    Remove unused button3 parameter from DzMessageBox methods

--- src/types/daz/dz_messageBox.d.ts
+++ src/types/daz/dz_messageBox.d.ts
@@ -3,8 +3,8 @@ declare class DzMessageBox extends QObject {
     /** Properties */
 
     /** Methods */
-    information(text: String, title: String, button0: String, button1?: String, button2?: String, button3?: String): number;
-    question(text: String, title: String, button0: String, button1?: String, button2?: String, button3?: String): number;
-    warning(text: String, title: String, button0: String, button1?: String, button2?: String, button3?: String): number;
-    critical(text: String, title: String, button0: String, button1?: String, button2?: String, button3?: String): number;
+    information(text: String, title: String, button0: String, button1?: String, button2?: String): number;
+    question(text: String, title: String, button0: String, button1?: String, button2?: String): number;
+    warning(text: String, title: String, button0: String, button1?: String, button2?: String): number;
+    critical(text: String, title: String, button0: String, button1?: String, button2?: String): number;
 }
